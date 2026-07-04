0e49aa8ed389f542eb64e42e42ed283b0f42f2c4
erxykurt <erxykurt@gmail.com>
2026-07-04 22:09:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,120 @@
+const summonBtn = document.getElementById("summonBtn");
+const summonScreen = document.getElementById("summonScreen");
+const container = document.querySelector(".container");
+
+container.style.display = "none";
+
+summonBtn.addEventListener("click", () => {
+
+    helloAudio.play();
+
+    summonScreen.style.display = "none";
+
+    container.style.display = "block";
+
+});
+
+const birthday=document.getElementById("birthday");
+const helloAudio = document.getElementById("helloAudio");
+
+const items = [
+    document.querySelector(".character"),
+    document.querySelector(".container h1"),
+    document.querySelector(".container p"),
+    document.querySelector("textarea"),
+    document.getElementById("completeButton")
+];
+
+summonBtn.addEventListener("click", () => {
+
+    helloAudio.play();
+
+    summonScreen.style.display = "none";
+
+    container.style.display = "block";
+
+    items.forEach((item, index) => {
+
+        setTimeout(() => {
+
+            item.classList.add("show");
+
+        }, index * 300);
+
+    });
+
+});
+
+let played = false;
+
+function playHello(){
+
+    if(played) return;
+
+    helloAudio.play();
+
+    played = true;
+
+}
+
+// Telefonlarda ilk dokunuşta sesi çalıştırır
+document.addEventListener("click", playHello, { once:true });
+
+document.addEventListener("touchstart", playHello, { once:true });
+
+const task = document.getElementById("task");
+
+task.addEventListener("input", ()=>{
+
+    task.style.boxShadow="0 0 35px #33ddff";
+
+});
+const button=document.getElementById("completeButton");
+const character=document.querySelector(".character");
+const completed=document.getElementById("completed");
+
+const puffAudio=document.getElementById("puffAudio");
+
+button.addEventListener("click",()=>{
+
+    button.disabled=true;
+
+    button.innerText="COMPLETING TASK...";
+
+    setTimeout(()=>{
+
+        puffAudio.play();
+
+        character.classList.add("hide");
+
+        button.classList.add("hide");
+
+        task.classList.add("hide");
+
+        document.querySelector("h1").classList.add("hide");
+
+        document.querySelector("p").classList.add("hide");
+
+        setTimeout(()=>{
+
+    completed.classList.add("show");
+
+setTimeout(()=>{
+
+    document.body.classList.add("fade");
+
+    setTimeout(()=>{
+
+        document.body.classList.remove("fade");
+
+        birthday.classList.add("show");
+
+    },800);
+
+},2000);
+
+},700);
+
+    },1500);
+
+});
